ce8dbe32a22821df377799edec75a1dfb64ae2eb
npcxl <18569795073@163.com>
2026-07-10 16:21:05 +0800

    chore: 移除废弃的SQL迁移脚本

--- bls-server/sql/migration-data-scope.sql
+++
@@ -1,4 +0,0 @@
--- P9: sys_role 添加 data_scope 字段
-ALTER TABLE `sys_role` ADD COLUMN IF NOT EXISTS `data_scope` varchar(20) NOT NULL DEFAULT 'TENANT'
-  COMMENT '数据权限范围: ALL/TENANT/DEPT/DEPT_AND_CHILDREN/SELF/CUSTOM'
-  AFTER `role_key`;
